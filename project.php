7b1b19cee0e264bbacd05fe1f0bc8cedacc79fa0
hannahbishop <hannahbananabishop@gmail.com>
2017-01-17 08:47:26 -0800

    Revert "renaming plugin"
    This reverts commit 4333b71dd15525964c37ee485ed2d12f8958889b.

--- project.php
+++ project.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Project
+Plugin Name: Projects
 Description: Declares a plugin that will create a project post type. Modified from https://www.smashingmagazine.com/2012/11/complete-guide-custom-post-types/
 Version: 1.0
 Author: Hannah Bishop
@@ -33,4 +33,5 @@ function post_type_project() {
   register_post_type( 'project', $args );
 }
 add_action( 'init', 'post_type_project' );
+add_post_type_support( 'project', 'post-formats' );
 ?>
